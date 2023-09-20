0402a8584bd20b24b51a8dc98bb48d3deaa0f85a
jesx <jeresergor@gmail.com>
2023-09-20 17:47:17 -0600

    Se agregó el arlgoritmo de notación infija a prefija

--- alg_pre.java
+++ alg_pre.java
@@ -1,3 +1,63 @@
-public class alg_pre{
+import java.util.Stack;
 
+public class alg_pre {
+    public static String infixToPrefix(String expression) {
+
+        expression = expression.replaceAll("\\s+", "");
+        expression = "(" + expression + ")";
+        
+        Stack<Character> operators = new Stack<>();
+        Stack<String> operands = new Stack<>();
+        
+        for (int i = 0; i < expression.length(); i++) {
+            char ch = expression.charAt(i);
+            
+            if (ch == '(') {
+                operators.push(ch);
+            } else if (ch == ')') {
+                while (!operators.isEmpty() && operators.peek() != '(') {
+                    char operator = operators.pop();
+                    String operand2 = operands.pop();
+                    String operand1 = operands.pop();
+                    String result = operator + operand1 + operand2;
+                    operands.push(result);
+                }
+                operators.pop();
+            } else if (isOperator(ch)) {
+                while (!operators.isEmpty() && precedence(ch) <= precedence(operators.peek())) {
+                    char operator = operators.pop();
+                    String operand2 = operands.pop();
+                    String operand1 = operands.pop();
+                    String result = operator + operand1 + operand2;
+                    operands.push(result);
+                }
+                operators.push(ch);
+            } else {
+                
+                StringBuilder operand = new StringBuilder();
+                while (i < expression.length() && (Character.isLetterOrDigit(expression.charAt(i)) || expression.charAt(i) == '.')) {
+                    operand.append(expression.charAt(i));
+                    i++;
+                }
+                i--; 
+                operands.push(operand.toString());
+            }
+        }
+        
+        return operands.pop();
+    }
+    
+    private static boolean isOperator(char ch) {
+        return ch == '+' || ch == '-' || ch == '*' || ch == '/';
+    }
+    
+    private static int precedence(char ch) {
+        if (ch == '+' || ch == '-') {
+            return 1;
+        } else if (ch == '*' || ch == '/') {
+            return 2;
+        }
+        return 0;
+    }
+    
 }
--- main.java
+++ main.java
@@ -2,5 +2,11 @@ public class main{
     public static void main(String[] args) {
         alg_post ap = new alg_post();
         ap.ejecutarAlgoritmo();
+
+        alg_pre p = new alg_pre();
+        String infixExpression = "2+4/5*(5-3)-5*4";
+        String prefixExpression = p.infixToPrefix(infixExpression);
+        System.out.println("Expresión infija: " + infixExpression);
+        System.out.println("Expresión prefija: " + prefixExpression);
     }
 }
